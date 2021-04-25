24a50de90ffb036663c43290025dcdc7e8272d69
zachliu <freedream0511@gmail.com>
2021-04-25 23:13:25 +0800

    增加大眼滤镜

--- app/src/main/java/com/example/openglcamerademo/utils/CameraHelper.java
+++ app/src/main/java/com/example/openglcamerademo/utils/CameraHelper.java
@@ -1,24 +1,40 @@
 package com.example.openglcamerademo.utils;
 
+import android.os.Handler;
 import android.os.HandlerThread;
 import android.util.Size;
 
 import androidx.camera.core.CameraX;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageAnalysisConfig;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.core.PreviewConfig;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.OnLifecycleEvent;
 
-public class CameraHelper {
+import com.example.openglcamerademo.face.Face;
+import com.example.openglcamerademo.face.FaceTracker;
+import com.example.openglcamerademo.ui.ImageUtils;
+
+public class CameraHelper implements ImageAnalysis.Analyzer, LifecycleObserver {
 
     private HandlerThread handlerThread;
     private CameraX.LensFacing currentFacing = CameraX.LensFacing.BACK;
     private Preview.OnPreviewOutputUpdateListener listener;
 
+    private FaceTracker faceTracker;
+    private Face face;
+
     public CameraHelper(LifecycleOwner lifecycleOwner, Preview.OnPreviewOutputUpdateListener listener) {
         this.listener = listener;
+        lifecycleOwner.getLifecycle().addObserver(this);
+
         handlerThread = new HandlerThread("Analyze-Thread");
         handlerThread.start();
-        CameraX.bindToLifecycle(lifecycleOwner, getPreView());
+        CameraX.bindToLifecycle(lifecycleOwner, getPreView(), getImageAnalysis());
     }
 
     private Preview getPreView() {
@@ -31,4 +47,53 @@ public class CameraHelper {
         preview.setOnPreviewOutputUpdateListener(listener);
         return preview;
     }
+
+    private ImageAnalysis getImageAnalysis() {
+        ImageAnalysisConfig imageAnalysisConfig = new ImageAnalysisConfig.Builder()
+                .setCallbackHandler(new Handler(handlerThread.getLooper()))
+                .setLensFacing(currentFacing)
+                .setTargetResolution(new Size(640, 480))
+                .setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
+                .build();
+        ImageAnalysis imageAnalysis = new ImageAnalysis(imageAnalysisConfig);
+        imageAnalysis.setAnalyzer(this);
+        return imageAnalysis;
+
+    }
+
+    @Override
+    public void analyze(ImageProxy image, int rotationDegrees) {
+        byte[] bytes = ImageUtils.getBytes(image, rotationDegrees, image.getWidth(), image.getHeight());
+
+        synchronized (this) {
+            face = faceTracker.detect(bytes, image.getWidth(), image.getHeight(), rotationDegrees);
+        }
+    }
+
+    public synchronized Face getFace() {
+        return face;
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    public void onCreate(LifecycleOwner owner) {
+        faceTracker = new FaceTracker("/sdcard/lbpcascade_frontalface.xml", "/sdcard/pd_2_00_pts5.dat");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    public void onStart(LifecycleOwner owner) {
+        faceTracker.start();
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    public void onStop(LifecycleOwner owner) {
+        faceTracker.stop();
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    public void onDestroy(LifecycleOwner owner) {
+        faceTracker.release();
+        owner.getLifecycle().removeObserver(this);
+    }
+
+
 }
